5df75cb0b8bfb8b7e06325855794a0d71e463821
Adam Verma <vermaadam@gmail.com>
2019-12-21 03:07:30 -0600

    Relocated the html routes from server.js, Added Submit Survey and Post to Data Info

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = function() { 
+// Route to send user by AJAX request to 'home' page 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
+
+// Route to send user by AJAX request to 'survey' page 
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+});
+}
--- server.js
+++ server.js
@@ -1,2 +1,20 @@
+// Dependencies
 const express = require('express');
 const path = require('path');
+
+// Require the routes' files so data can be imported
+let apiRoutes = require('./app/routing/apiRoutes');
+let htmlRoutes = require('./app/routing/htmlRoutes');
+
+// Sets up the Express App
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+// Handles the data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Use the routes' files 
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
